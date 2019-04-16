b5c5d75ae93a2228861001967c8be933bca3eac1
onefen123 <3006139@qq.com>
2019-04-16 10:44:15 +0800

    1

--- src/main/java/net/fen/shop/admin/controller/IndexController.java
+++ src/main/java/net/fen/shop/admin/controller/IndexController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String index(){
-        return "admin/index";
+        return "/admin/index";
     }
 }
